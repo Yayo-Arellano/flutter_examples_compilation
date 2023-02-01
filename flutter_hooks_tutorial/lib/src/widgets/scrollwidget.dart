dec246f962cd16ce01ea1da32497162b969a8f28
Yayo Arellano <>
2023-02-01 11:49:13 +0800

    Update examples to Flutter 3.7.0

--- flutter_hooks_tutorial/lib/src/widgets/scrollwidget.dart
+++ flutter_hooks_tutorial/lib/src/widgets/scrollwidget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluttecr_hooks/flutter_hooks.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 final _red = new GlobalKey();
 final _blue = new GlobalKey();
--- simple_news_app_flutter_taipei/generate_code.sh
+++ simple_news_app_flutter_taipei/generate_code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-fvm global 2.10.3
+fvm global 3.7.0
 flutter clean
 flutter pub upgrade
 flutter pub run build_runner watch --delete-conflicting-outputs
