989afb681cfd01affc491085635440ba3cbc35d7
Yayo Arellano <gerardo_28@hotmail.com>
2025-02-21 16:04:46 +0800

    updated flutter_flavors_ios_android

--- flutter_flavors_ios_android/lib/environment/environment.dart
+++ flutter_flavors_ios_android/lib/environment/environment.dart
@@ -3,6 +3,8 @@ enum Environment {
     iOSBannerId: 'ca-app-pub-3940256099942544/2934735716',
     androidBannerId: 'ca-app-pub-3940256099942544/6300978111',
   ),
+  // Trying to prod in iOS without setting the correct ID's here and in the
+  // Info-Prod.plist file will crash the app.
   prod(
     iOSBannerId: 'xxx',
     androidBannerId: 'xxx',
